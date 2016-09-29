5e6d8a5e14bfe9aa259b94bb254ab9acb51cc240
Andreas Karrer <andreas.karrer@hsr.ch>
2016-09-29 20:43:45 +0200

    use strict and lambda syntax for simple functions

--- js/notes.js
+++ js/notes.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var notes =
     [{
         "id": 1,
@@ -44,16 +46,16 @@ function renderNotesList () {
     var orderby = $("input:radio[name ='orderby']:checked").val();
     var comparator;
     switch (orderby) {
-        case "due":        comparator = function(a, b) { return a.due < b.due; }; break;
-        case "created":    comparator = function(a, b) { return a.created < b.created; }; break;
-        case "importance": comparator = function(a, b) { return b.importance - a.importance; }; break;
+        case "due":        comparator = (a, b) => (a.due < b.due); break;
+        case "created":    comparator = (a, b) => (a.created < b.created); break;
+        case "importance": comparator = (a, b) => (b.importance - a.importance); break;
     }
 
     // make a filter function according to the "Show finished" checkbox
-    var today = new Date().toJSON().slice(0,10); // todays date in format yyyy-mm-dd
-    var showFinished = function(a) { return a.due > today; }
+    var today = new Date().toJSON().slice(0,10); // today's date in format yyyy-mm-dd
+    var showFinished = (a) => (a.due > today);
     if ($("#showfinished").is(':checked')) {
-        showFinished = function(a) { return true; }
+        showFinished = (a) => (true);
     }
 
     // notes list is first filtered and then sorted
