ddb9948882b8f1dfd6e3c6c6a1ed96a8f0b27844
Andreas Karrer <andreas.karrer@hsr.ch>
2016-11-09 03:04:29 +0100

    cosmetics

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,14 @@ var router = express.Router();
 var notesController = require('../controller/notesController.js');
 
 // for /, render the list
-router.get('/',         notesController.showIndex);
+router.get('/',          notesController.showIndex);
 
-router.get('/note',     notesController.showEditNote);
-router.get('/note/:id', notesController.showEditNote);
+router.get('/note',      notesController.showEditNote);
+router.get('/note/:id',  notesController.showEditNote);
 router.post('/note/:id', notesController.updateNote);
-router.post('/note/',   notesController.addNote);
+router.post('/note/',    notesController.addNote);
 
-router.get('/notes',    notesController.getNotes);
-router.post('/notes',   notesController.modifyNotesData);
+router.get('/notes',     notesController.getNotes);
+router.post('/notes',    notesController.modifyNotesData);
 
 module.exports = router;
